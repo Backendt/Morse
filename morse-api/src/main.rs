c8f13a3106cf77f6c71c2cdb3bd9b445d434a982
Backendt <matisse.beckandt@proton.me>
2024-11-12 15:01:49 +0100

    Made api host & port configurable

--- morse-api/src/main.rs
+++ morse-api/src/main.rs
@@ -5,15 +5,21 @@ mod models;
 mod repositories;
 mod database;
 
+use std::net::SocketAddr;
+
 #[tokio::main]
 async fn main() {
     let database = database::get_connection().await
         .expect("Could not connect to database.");
 
+    let socket: SocketAddr = std::env::var("LISTENING_SOCKET")
+        .unwrap_or_else(|_| String::from("0.0.0.0:8080"))
+        .parse().expect("Cannot parse the listening socket. Check your LISTENING_SOCKET environment variable");
+
     let api_routes = routes::get_routes();
 
-    println!("Running API on 127.0.0.1:8080..");
+    println!("Running API on {}:{}..", socket.ip(), socket.port());
     warp::serve(api_routes)
-        .run(([127, 0, 0, 1], 8080)) // TODO Configure listening host and port from env variables
+        .run(socket)
         .await;
 }
