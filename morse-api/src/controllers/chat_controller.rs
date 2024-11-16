f21fbb5a114c6cced18e9043002e91e01c8f45dc
Backendt <matisse.beckandt@proton.me>
2024-11-17 00:21:06 +0100

    Added json messages

--- morse-api/src/controllers/chat_controller.rs
+++ morse-api/src/controllers/chat_controller.rs
@@ -1,59 +1,56 @@
 use warp::ws::{WebSocket, Message};
-use futures::StreamExt;
-use tokio::sync::mpsc::{
-    unbounded_channel,
-    UnboundedReceiver,
-    UnboundedSender
-};
+use futures::{StreamExt, SinkExt};
+use tokio::sync::mpsc::UnboundedSender;
 use std::sync::Arc;
 
 use crate::{
     services::ws_service,
-    models::ws::UsersChannels
+    models::ws::{
+        UsersChannels,
+        WsMessage,
+        WsStatus,
+        Messageable
+    }
 };
 
 pub async fn on_client_connect(username: String, socket: WebSocket, users: Arc<UsersChannels>) {
-    let (sender, mut receiver) = socket.split();
+    let (mut sender, mut receiver) = socket.split();
+    
+    match ws_service::add_client(&username, &users).await {
+        Err(error_message) => {
+            let _ = sender.send(error_message.as_message());
+        },
+        Ok((user_sender, user_receiver)) => {
+            ws_service::start_forwarding(user_receiver, sender).await;
 
-    let (user_sender, user_receiver) = add_client(&username, &users).await; 
-    ws_service::start_forwarding(user_receiver, sender).await;
+            while let Some(raw_message) = receiver.next().await { // TODO Put in its own function
+                let parsed_message = match raw_message {
+                    Ok(message) => ws_service::parse_message(message),
+                    Err(err) => { // TODO Handle Protocol(ResetWithoutClosingHandshake) when client closes
+                        eprintln!("Could not receive message from {username}: {err:?}");
+                        break;
+                    }
+                };
 
-    while let Some(content) = receiver.next().await {
-        match content {
-            Ok(msg) => on_message(&username, msg, user_sender.clone(), &users).await,
-            Err(err) => { // Protocol(ResetWithoutClosingHandshake) when client closes
-                eprintln!("Could not read message from {username}: {err:?}");
-                break; 
+                match parsed_message {
+                    Ok(message) => on_message(&username, message, user_sender.clone(), &users).await,
+                    Err(error_message) => { 
+                        let _ = user_sender.send(error_message.as_message());
+                    }
+                };
             }
-        };
+            ws_service::remove_client(&username, &users).await;
+        }
+    };
+}
+
+async fn on_message(username: &String, message: WsMessage, user_channel: UnboundedSender<Message>, users: &Arc<UsersChannels>) {
+    println!("Received from {username}: {message:?}");
+    match message.action.as_str() {
+        "send_message" => ws_service::send_message(username, message, user_channel, users).await,
+        _ => {
+            let error_message = WsStatus::new("error", "Invalid action").as_message();
+            let _ = user_channel.send(error_message);
+        }
     }
-
-    remove_client(&username, &users).await;
-}
-
-async fn add_client(username: &String, users: &Arc<UsersChannels>) -> (UnboundedSender<Message>, UnboundedReceiver<Message>) {
-    // TODO Check if already connected
-    println!("[+] '{username}' connected to websocket.");
-    let (user_sender, user_receiver) = unbounded_channel();
-    users.write().await.insert(username.clone(), user_sender.clone());
-    (user_sender, user_receiver)
-}
-
-async fn remove_client(username: &String, users: &Arc<UsersChannels>) {
-    println!("[-] '{username}' disconnected from websocket.");
-    users.write().await.remove(username);
-}
-
-// TODO Impl made for testing
-async fn on_message(username: &String, message: Message, user_sink: UnboundedSender<Message>, users: &Arc<UsersChannels>) {
-    let (target, text) = message.to_str().unwrap().split_once("@").expect("Specify target");
-
-    let response = Message::text(
-        format!("[{username} -> {target}]): {text}")
-    );
-    let _ = user_sink.send(response.clone());
-
-    let sinks = users.read().await;
-    let target_sink = sinks.get(target).expect("User not found");
-    let _ = target_sink.send(response);
 }
--- morse-api/src/models/ws.rs
+++ morse-api/src/models/ws.rs
@@ -10,10 +10,39 @@ use warp::ws::{WebSocket, Message};
 pub type WsSink = SplitSink<WebSocket, Message>;
 pub type UsersChannels = RwLock<HashMap<String, mpsc::UnboundedSender<Message>>>;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct WsMessage {
     pub action: String,
     pub target: String,
-    pub content: Vec<u8>
+    pub content: String
+}
+impl Messageable for WsMessage {}
+
+#[derive(Debug, Serialize)]
+pub struct WsStatus {
+    pub status: String,
+    pub message: String
+}
+impl Messageable for WsStatus {}
+
+impl WsStatus {
+    pub fn new(status: &str, message: &str) -> Self {
+        Self {
+            status: String::from(status),
+            message: String::from(message)
+        }
+    }
+
+    pub fn err<T>(message: &str) -> Result<T, Self> {
+        Err(Self::new("error", message))
+    }
+}
+
+pub trait Messageable: Serialize {
+    fn as_message(&self) -> warp::ws::Message {
+        let as_text = serde_json::to_string(self)
+            .expect("Could not serialize message");
+        Message::text(as_text)
+    }
 }
 
